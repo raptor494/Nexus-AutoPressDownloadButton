22a56863d6ee71d21eb7e9f9763f28e0c964c669
raptor4694 <70232340+raptor4694@users.noreply.github.com>
2020-09-06 15:44:41 -0700

    Update README.md

--- AutoDownloadNexus.js
+++ AutoDownloadNexus.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.nexusmods.com/skyrimspecialedition/mods/*?tab=files&file_id=*&nmm=1
+// @match        https://www.nexusmods.com/*/mods/*?tab=files&file_id=*&nmm=1
 // @require     http://code.jquery.com/jquery-latest.js
 // @grant        none
 // ==/UserScript==
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Nexus-AutoPressDownloadButton
- 
+This is a userscript for the Nexus mod page which automatically presses the "Slow Download" button on the download page.
+You need a userscript manager such as Greasemonkey or Tampermonkey to use this.
+
+Tested with Tampermonkey+Firefox.
