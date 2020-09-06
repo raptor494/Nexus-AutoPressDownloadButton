30e65ca47de7048bc7ff7f68702b3f7fff69fe56
raptor4694 <70232340+raptor4694@users.noreply.github.com>
2020-09-06 15:43:09 -0700

    Initial commit

---
+++ AutoDownloadNexus.ts
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Auto-Download Nexus
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.nexusmods.com/skyrimspecialedition/mods/*?tab=files&file_id=*&nmm=1
+// @require     http://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+$(function() {
+    $('#slowDownloadButton').trigger('click')
+})
